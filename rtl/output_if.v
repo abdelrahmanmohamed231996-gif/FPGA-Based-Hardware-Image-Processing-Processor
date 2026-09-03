09d468eeab3853419ec142efc3f6412e274d4ff5
Abdelrahman Hamad <abdelrahmanmohamed231996@gmail.com>
2026-09-03 17:51:22 +0300

    Add files via upload

---
+++ rtl/output_if.v
@@ -0,0 +1,42 @@
+module out_if(rst_n,pixel_out,pixel_out_valid,mem_wr_en,mem_wr_addr,write_data,
+,write_en,write_addr);
+input wire rst_n,pixel_out_valid,mem_wr_en;
+input  wire [7:0] pixel_out,mem_wr_addr;
+output reg write_en;
+output reg [7:0] write_addr,write_data;
+
+always @(*) begin
+    if (~rst_n) begin
+        write_en=0;
+        write_addr=0;
+        write_data=0;
+        
+    end
+    else if (pixel_out_valid&&mem_wr_en) begin
+        write_en=1;
+        write_addr=mem_wr_addr;
+        write_data=pixel_out;
+
+
+
+        
+    end
+    else if(pixel_out_valid) begin
+              write_en=0;
+        write_addr=mem_wr_addr;
+        write_data=pixel_out;
+
+    end else  begin
+        write_en=0;
+        write_addr=0;
+        write_data=0;
+
+        
+    end
+    
+end
+
+
+
+
+endmodule
