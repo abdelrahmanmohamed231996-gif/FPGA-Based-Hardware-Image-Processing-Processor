5ca60172f7d0d43e97aab431fcabafc92267c283
Mohamed Emadeldin Ali <mohamed04emad@gmail.com>
2026-09-08 21:50:22 +0300

    bram

--- rtl/image_bram.v
+++ rtl/image_bram.v
@@ -1,19 +1,25 @@
 module image_bram (
-    input wire clk,
-    input wire [7:0] addr,
-    output reg [7:0] image_mem_top,
-    output reg [7:0] image_mem_mid,
-    output reg [7:0] image_mem_bot
-
+    input  wire       clk,
+    input  wire [7:0] addr_top,
+    input  wire [7:0] addr_mid,
+    input  wire [7:0] addr_bot,
+    output reg  [7:0] row_top,
+    output reg  [7:0] row_mid,
+    output reg  [7:0] row_bot
 );
   reg [7:0] image_mem_top[0:255];
   reg [7:0] image_mem_mid[0:255];
   reg [7:0] image_mem_bot[0:255];
 
-  initial $readmemh("image.mem", image_mem);
-
-  always @(posedge clk) begin
-    data_out <= image_mem[addr];
-
+  initial begin
+    $readmemh("image.mem", image_mem_top);
+    $readmemh("image.mem", image_mem_mid);
+    $readmemh("image.mem", image_mem_bot);
   end
+
+
+  always @(posedge clk) row_top <= image_mem_top[addr_top];
+  always @(posedge clk) row_mid <= image_mem_mid[addr_mid];
+  always @(posedge clk) row_bot <= image_mem_bot[addr_bot];
+
 endmodule
