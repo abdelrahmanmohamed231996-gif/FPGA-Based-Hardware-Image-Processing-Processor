8f4ddccddb92302bf50c2036327b79be7b6f0f85
RahmaEssamMahmoudi <109796436+RahmaEssamMahmoudi@users.noreply.github.com>
2026-09-22 02:02:19 +0300

    Expand kernel bank with additional filters
    Added new filters 'emboss', 'bottom_sobel', and 'outline' to the kernel bank.

--- modeling/get_kernel_bank.m
+++ modeling/get_kernel_bank.m
@@ -1,7 +1,7 @@
 function [bank, names] = get_kernel_bank()
 % Returns the fixed 3x3 filter bank used by the Golden Model.
 
-names = ["identity", "edge_h", "edge_v", "sharpen", "blur", "custom_edge"];
+names = ["identity", "edge_h", "edge_v", "sharpen", "blur", "custom_edge","emboss","bottom_sobel","outline"];
 
 bank = zeros(3, 3, 6, "int64");
 
@@ -28,4 +28,20 @@ bank(:,:,5) = [1 1 1;
 bank(:,:,6) = [ 1  2  1;
                 0  0  0;
                -1 -2 -1 ];
+
+bank(:,:,7) = int64([ ...
+    -2  -1   0;
+    -1   1   1;
+     0   1   2 ]);
+
+bank(:,:,8) = int64([ ...
+    -1  -2  -1;
+     0   0   0;
+     1   2   1 ]);
+
+bank(:,:,9) = int64([ ...
+    -1  -1  -1;
+    -1   8  -1;
+    -1  -1  -1 ]);
+
 end
